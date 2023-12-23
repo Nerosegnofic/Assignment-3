3ebee3914e80036afbecc470e643669dabdef73f
Ahmed Abdelnabi <77232129+Nerosegnofic@users.noreply.github.com>
2023-12-23 08:28:40 +0200

    Update X_O_Board.cpp

--- X_O_Games/src/X_O_Board.cpp
+++ X_O_Games/src/X_O_Board.cpp
@@ -1,3 +1,7 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
 // Class definition for X_O_Board class
 // Author:  Mohammad El-Ramly
 // Date:    10/10/2022

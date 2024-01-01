8d5ddb255227b6bc07889f194d7a88d3f2ae0794
nouraldein66 <111093819+nouraldein66@users.noreply.github.com>
2024-01-01 23:14:58 +0200

    Update AI_Player5x5.cpp

--- X_O_Games/src/AI_Player5x5.cpp
+++ X_O_Games/src/AI_Player5x5.cpp
@@ -2,7 +2,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-map <string,int> dp;
 
 AI_Player5x5::AI_Player5x5(char symbol, Board* board) : Player(symbol)
 {
@@ -11,7 +10,7 @@ AI_Player5x5::AI_Player5x5(char symbol, Board* board) : Player(symbol)
     boardptr = board;
 }
 
-string AI_Player5x5::getString(vector<string> t) {
+string AI_Player5x5::getString(vector<string>& t) {
     string s = "";
     for (int i = 0; i < boardptr->get_n_rows(); ++i) {
         for (int j = 0; j < boardptr->get_n_cols(); ++j) {
@@ -36,6 +35,7 @@ int AI_Player5x5::evaluatingFunction(vector<string>& Board, bool maximizer){
     //calculating x's and o's winning conditions
     int countX = 0, countO = 0;
     if(boardptr->game_is_over()){
+        cout << 0 << "\n";
         if(boardptr->is_draw())
             return 0;
         if(boardptr->is_winner()){
@@ -244,13 +244,13 @@ int AI_Player5x5::evaluatingFunction(vector<string>& Board, bool maximizer){
     }
     return maximizer ? -(countX - countO) : -(countO - countX);
 }
+
+map <string,int> dp;
 int AI_Player5x5::minimax(vector<string> &v, int depth, int alpha, int beta, bool computer_turn) {
-    string s = getString(v);
+    bool pruned = false;
     if(depth == 0){
         return evaluatingFunction(v, computer_turn);
     }
-    if(dp.find(s) != dp.end())
-        return dp[s];
 
     if(computer_turn){
         int maxEvaluation = -1e9;
@@ -258,18 +258,27 @@ int AI_Player5x5::minimax(vector<string> &v, int depth, int alpha, int beta, boo
             for (int j = 0; j < boardptr->get_n_cols(); ++j) {
                 if(!v[i][j]){
                     v[i][j] = 'O';
-                    int nextEval = minimax(v,depth-1,alpha,beta,false);
+                    int nextEval;
+                    string s = getString(v);
+                    if(dp.find(s) != dp.end()){
+                        nextEval = dp[s];
+                    }
+                    else
+                        nextEval = minimax(v,depth-1,alpha,beta,false);
                     v[i][j] = 0;
                     maxEvaluation = max(nextEval,maxEvaluation);
+                    if(!pruned)
+                        dp[s] = maxEvaluation;
                     alpha = max(alpha,maxEvaluation);
                     if(alpha >= beta){
+                        pruned = true;
                         break;
                     }
 
                 }
             }
         }
-        return dp[s] = maxEvaluation;
+        return maxEvaluation;
 
     }
 
@@ -278,23 +287,32 @@ int AI_Player5x5::minimax(vector<string> &v, int depth, int alpha, int beta, boo
         for (int i = 0; i < boardptr->get_n_rows(); ++i) {
             for (int j = 0; j < boardptr->get_n_cols(); ++j) {
                 if(!v[i][j]) {
+                    int nextEval;
                     v[i][j] = 'X';
-                    int nextEval = minimax(v,depth-1,alpha,beta,true);
+                    string s = getString(v);
+                    if(dp.find(s) != dp.end()) {
+                        nextEval = dp[s];
+                    }
+                    else
+                        nextEval = minimax(v,depth-1,alpha,beta,true);
                     v[i][j] = 0;
                     minEvaluation = min(nextEval, minEvaluation);
                     beta = min(beta, minEvaluation);
+                    if(!pruned)
+                        dp[s] = minEvaluation;
                     if (alpha >= beta) {
+                        pruned = true;
                         break;
                     }
                 }
             }
         }
-        return dp[s] = minEvaluation;
+        return minEvaluation;
     }
 }
 
 void AI_Player5x5::get_move(int &x, int &y) {
-    int  max_eval{(int)-1e9}, min_eval{(int) 1e9} , alpha{(int)-1e9} , beta{(int)1e9};
+    int  max_eval{(int)-1e9}, min_eval{(int) 1e9} , alpha{(int)-1e9} , beta{(int)1e9}, nextScore;
     vector<string> temp_board(boardptr->get_n_rows());
 
     temp_board = boardptr->get_board();
@@ -302,7 +320,11 @@ void AI_Player5x5::get_move(int &x, int &y) {
         for (int j = 0; j < boardptr->get_n_cols(); ++j) {
             if(!temp_board[i][j]){
                 temp_board[i][j] = 'O';
-                int nextScore = minimax(temp_board,5,alpha,beta,false);
+                string s = getString(temp_board);
+                if(dp.find(s) != dp.end())
+                    nextScore = dp[s];
+                else
+                    nextScore = minimax(temp_board,5,alpha,beta,false);
                 if(nextScore > max_eval){
                     max_eval = nextScore;
                     x = i;
